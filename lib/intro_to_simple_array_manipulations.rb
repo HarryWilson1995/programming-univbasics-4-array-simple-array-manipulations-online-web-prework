b03699ea3cf2d853e253d13649922bf040c5ebce
Harry Wilson <harry.wilson1995a@gmail.com>
2019-11-19 17:31:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array, array2)
   array.concat(array2)
 end
 
-def using_insert(array, element)
-  array.insert(element)
+def using_insert(array, string)
+  array.insert(string)
 end
 
 def using_uniq(array)
